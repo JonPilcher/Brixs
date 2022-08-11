c9d6edb3ff9dca219f612b85c0ae2ec9b13baf1a
Jon <mrjpilcher@gmail.com>
2022-08-11 11:31:44 +1000

    Changed bricks to 18 wide and moved to center

--- Engine/Brick.h
+++ Engine/Brick.h
@@ -12,6 +12,7 @@ public:
 	void Draw(Graphics& gfx)const;
 	bool DoBallCollision(Ball& ball);
 private:
+	static constexpr float padding = 1.0f;
 	Rectf rect;
 	Color color;
 	bool destroyed = false;
