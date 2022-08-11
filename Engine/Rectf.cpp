e1e99ccf64a424453942d1be516de4be1446b42f
Jon <mrjpilcher@gmail.com>
2022-08-11 14:54:10 +1000

    added find center of rect to Rectf

---
+++ Engine/Rectf.cpp
@@ -0,0 +1,44 @@
+#include "Rectf.h"
+
+Rectf::Rectf(float left_in, float right_in, float top_in, float bottom_in)
+{
+	left = left_in;
+	right = right_in;
+	top = top_in;
+	bottom = bottom_in;
+}
+
+Rectf::Rectf(const Vec2& topLeft, const Vec2 bottomRight)
+	:
+	Rectf(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y)
+{
+
+}
+
+Rectf::Rectf(const Vec2 topLeft, float width, float height)
+	:
+	Rectf(topLeft,topLeft + Vec2(width,height))
+{
+}
+
+bool Rectf::isOverLappingWith(const Rectf& other) const
+{
+	return right > other.left && left < other.right
+		&& bottom > other.top && top < other.bottom;
+}
+
+Rectf Rectf::FromCenter(const Vec2& center, float halfWidth, float halfHeight)
+{
+	const Vec2 half(halfWidth, halfHeight);
+	return Rectf(center - half,center + half);
+}
+
+Rectf Rectf::GetExpanded(float offset) const
+{
+	return Rectf(left - offset,right + offset,top - offset,bottom + offset);
+}
+
+Vec2 Rectf::GetCenter() const
+{
+	return Vec2((left + right) / 2.0f, (top + bottom) / 2.0f);
+}
---
+++ Engine/Rectf.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Vec2.h"
+
+class Rectf
+{
+public:
+	Rectf() = default;
+	Rectf(float left_in, float right_in, float top_in, float bottom_in);
+	Rectf(const Vec2& topLeft, const Vec2 bottomRight);
+	Rectf(const Vec2 topLeft, float width, float height);
+	bool isOverLappingWith(const Rectf& other)const;
+	static Rectf FromCenter(const Vec2& center, float halfWidth, float halfHeight);
+	Rectf GetExpanded(float offset)const;
+	Vec2 GetCenter()const;
+public:
+	float left;
+	float right;
+	float top;
+	float bottom;
+};
