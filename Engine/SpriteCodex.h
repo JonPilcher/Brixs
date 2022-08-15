c46e44f2772f68c25d2fe3f91d84b2c26cc0745f
Jon <mrjpilcher@gmail.com>
2022-08-15 22:56:53 +1000

    sprite codex Updated

--- Engine/SpriteCodex.h
+++ Engine/SpriteCodex.h
@@ -8,6 +8,7 @@ class SpriteCodex
 public:
 	// centered drawing of 14x14 sprite
 	static void DrawBall( const Vec2& center,Graphics& gfx );
+	static void DrawBallSB(const int x, const int y, Graphics& gfx);
 	// 300px by 150px
 	static void DrawTitle(const int x,const int y, Graphics& gfx);
 	static void DrawStartMessage(const int x, const int y, Graphics& gfx);
